f7224d1172572246e3b20727e800e83b0ecb93e0
DrBryanMan <120161900+DrBryanMan@users.noreply.github.com>
2025-02-18 16:48:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,26 @@ import * as Template from './templates.js'
 const router = new Navigo('/', { hash: true })
 
 router
-    .on('/articles/characters/:character', ({ data }) => {
-        renderPage('characters', data.character)
+    .on('/articles/characters/:character', (match) => {
+        // console.log(data.character)
+        renderPage('characters', match.data.character)
     })
-    .on('/articles/comics/:comics', ({ data }) => {
-        const comics_name = data.comics.split(' Том')[0] // Дивовижна Людина-павук
-        let vol = data.comics.split('Том ')[1] // 1_1 або 1
+    .on('/articles/comics/:comics', (match) => {
+        // pagename = data.comics
+        const comics_name = match.data.comics.split(' Том')[0] // Дивовижна Людина-павук
+        let vol = match.data.comics.split('Том ')[1] // 1_1 або 1
         let issue
         if (vol.length > 1) {
             [vol, issue] = vol.split(' ')
             console.log(vol, issue)
-            renderPageData('issue', data.comics, comics_name, vol, issue)
+            renderPageData('issue', match.data.comics, comics_name, vol, issue)
         } else {
-            renderPageData('comics', data.comics, comics_name, vol)
+            renderPageData('comics', match.data.comics, comics_name, vol)
         }
     })
+    // .on('/articles/comics/:issue', ({ data }) => {
+    //     renderPage(`comics`, data.issue)
+    // })
     .resolve()
 
 async function loadFile(path) {
@@ -38,6 +43,7 @@ async function loadData(category, pagename, comics_name, vol, issue) {
     let jsonPath
     let mdPath
     if (vol) {
+        // const trimtitle = pagename.substring(0, pagename.lastIndexOf(' '))
         if (issue) {
             mdPath = `./articles/comics/${comics_name}/${comics_name} Том ${vol} ${issue}.md`
         } else {
@@ -60,6 +66,8 @@ function creatPageHeader(pagename, comics_name, vol, issue) {
     const page = document.getElementById('page')
     const pageHeader = document.createElement('div')
     pageHeader.id = 'pageHeader'
+    // pageHeader.innerHTML = ''
+    // const series_num = pagename.substring(0, pagename.lastIndexOf(' '))
     pageHeader.innerHTML = `
         <div>Категорії: ${comics_name}</div>
         <h1>${pagename}</h1>
@@ -82,13 +90,20 @@ async function renderPageData(category, pagename, comics_name, vol, issue) {
     }
     switch (category) {
         case 'comics':
+            // renderComicsPage(pageData, pagename, comics_name, vol)
             Template.Comics(pageData, pagename, comics_name, vol)
             Template.ComicsList(pageData, pagename, comics_name, vol)
             break
             
             case 'issue':
+            // renderIssuePage(pageData, pagename, comics_name, vol, issue)
             Template.Issue(pageData, pagename, comics_name, vol, issue)
             break
     }
     creatPageHeader(pagename, comics_name, vol, issue)
 }
+
+// function renderComicsPage(pageData, pagename, comics_name, vol) {
+// }
+// function renderIssuePage(pageData, pagename, comics_name, vol, issue) {
+// }
